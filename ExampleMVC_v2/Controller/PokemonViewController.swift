eba1c903e25924ec5ffe11641cfe66630bcc6a77
Moisés Sánchez <moy.sav21@hotmail.com>
2022-09-03 00:39:52 -0500

    feat: :sparkles: header collection view with label

--- ExampleMVC_v2/Controller/PokemonViewController.swift
+++ ExampleMVC_v2/Controller/PokemonViewController.swift
@@ -41,11 +41,21 @@ class PokemonViewController: UIViewController, UICollectionViewDataSource, UICol
         selectPokemon = manager.pokemonAtIndex(index: indexPath.row)
         self.performSegue(withIdentifier: "PokemonDetailSegue", sender: Self.self)
     }
-    /*
+    
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-            let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header", for: indexPath)
-            return header
-    }*/
+        switch kind {
+            case UICollectionView.elementKindSectionHeader:
+                if let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HeaderReusableView", for: indexPath) as? HeaderCollectionReusableView {
+                        header.titleLabel.text = "Pokemon"
+                        return header
+                    }
+            default:
+                return UICollectionReusableView()
+            
+        }
+        return UICollectionReusableView()
+        
+    }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destination = segue.destination as! PokemonDetailViewController
---
+++ ExampleMVC_v2/View/HeaderCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderCollectionReusableView.swift
+//  ExampleMVC_v2
+//
+//  Created by DISMOV on 03/09/22.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+        
+    @IBOutlet var titleLabel: UILabel!
+}
